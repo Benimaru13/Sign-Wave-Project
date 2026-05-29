e6c7587ec62a3ce6f93b3a2d09f8ba5c3f441c9f
Chibueze <benprichi13@gmail.com>
2026-05-29 15:02:54 -0700

    trying to fix the issue with motor server

--- motor_server.py
+++ motor_server.py
@@ -35,18 +35,18 @@ def main():
             command = data.decode().strip()
             print(f"  Received '{command}' from {addr[0]}")
             
-            if command == "FORWARD":
+            if command == "ON":
                 # Change output
                 PWD.set_motor_model(1000,1000,1000,1000)
                 print("Car moving forward")
-            elif command == "BACKWARD":
+            elif command == "OFF":
                 # Change output
                 PWD.set_motor_model(-1000,-1000,-1000,-1000)
                 print("Car moving backward")
                 
-            elif command == "OFF":
+            '' elif command == "OFF":
                 PWD.set_motor_model(0,0,0,0)
-                print("Car stopped")
+                print("Car stopped") ''
 
     except KeyboardInterrupt:
         print("\n  Shutting down...")
